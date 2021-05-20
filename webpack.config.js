a9b0226a139ece0946077347fde97483e9f3f7d6
Arnav Gupta <arnav@codingblocks.com>
2021-05-20 09:10:11 +0530

    setup: add @ and ~ alias via webpack as well
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const webpack = require("@nativescript/webpack");
+const { resolve } = require('path')
 
 module.exports = (env) => {
 	webpack.init(env);
 
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
+
+	webpack.chainWebpack(config => {
+		// change the "@" alias to "app/libs"
+		config.resolve.alias.set('@', resolve(__dirname, 'app'))
+		config.resolve.alias.set('~', resolve(__dirname, 'app'))
+
+	  })
 
 	return webpack.resolveConfig();
 };
